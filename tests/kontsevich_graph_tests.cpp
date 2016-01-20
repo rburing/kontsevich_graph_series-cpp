4d5ab2e522c163e40dff9193dfe61488246f71b0
Ricardo Buring <ricardo.buring@gmail.com>
2016-01-20 13:38:58 +0100

    More human-readable example.

--- tests/kontsevich_graph_tests.cpp
+++ tests/kontsevich_graph_tests.cpp
@@ -6,10 +6,12 @@ int main()
 {
     KontsevichGraph g(2, 2, { {0, 1}, {0, 2} });
     cout << g << "\n";
-    cout << "Targets: \n";
+    cout << "Edges: ";
     for (size_t v : g.internal_vertices())
     {
         std::pair<size_t, size_t> targets = g.targets(v);
-        cout << v << ": " << targets.first << " " << targets.second << "\n";
+        cout << "(" << v << ", " << targets.first << ", 'L') ";
+        cout << "(" << v << ", " << targets.second<< ", 'R') ";
     }
+    cout << "\n";
 }
