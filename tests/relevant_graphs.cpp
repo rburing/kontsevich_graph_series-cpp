d82546eea48d274de26df3da37fd6eaec6889c41
Ricardo Buring <ricardo.buring@gmail.com>
2016-06-01 16:35:40 +0200

    In relevant_graphs test, print angle brackets in usage message.

--- tests/relevant_graphs.cpp
+++ tests/relevant_graphs.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        cout << "Usage: " << argv[0] << " [order]\n";
+        cout << "Usage: " << argv[0] << " <order>\n";
         return 1;
     }
     size_t order = stoi(argv[1]);
