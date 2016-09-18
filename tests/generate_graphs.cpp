7cdf102bd3c644a699c7b15d5ebb67b79f2678e8
Ricardo Buring <ricardo.buring@gmail.com>
2016-09-18 17:08:42 +0200

    Slightly generalize generate_graphs

--- tests/generate_graphs.cpp
+++ tests/generate_graphs.cpp
@@ -5,24 +5,28 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc != 2 && argc != 3)
     {
-        cout << "Usage: " << argv[0] << " <order>\n";
+        cout << "Usage: " << argv[0] << " <internal> [external]\n";
         return 1;
     }
-    size_t order = stoi(argv[1]);
-    set<KontsevichGraph> relevants = KontsevichGraph::graphs(order, 2, true, true,
+    size_t internal = stoi(argv[1]);
+    size_t external = 2;
+    if (argc == 3)
+        external = stoi(argv[2]);
+
+    set<KontsevichGraph> graphs = KontsevichGraph::graphs(internal, external, true, true,
                     [](KontsevichGraph g) -> bool
                     {
                         return g.positive_differential_order() && g.is_prime();
                     });
     size_t counter = 0;
-    for (KontsevichGraph const& g : relevants)
+    for (KontsevichGraph const& g : graphs)
     {
         cout << g.encoding() << "    ";
         if (g.is_zero())
             cout << "0\n";
         else
-            cout << "w_" << order << "_" << (++counter) << "\n";
+            cout << "w_" << internal << "_" << (++counter) << "\n";
     }
 }
