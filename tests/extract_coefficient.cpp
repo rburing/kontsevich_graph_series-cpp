cd39249b786168b41df33dfa7efba98d1335b266
Ricardo Buring <ricardo.buring@gmail.com>
2017-01-28 17:36:53 +0100

    Clarify usage of extract_coefficient
    Make it clear that coefficients of arbitrary expressions can be
    extracted from graph series. That is, one may request not only
        $ extract_coefficient star_product4.txt w_2_1
    but also
        $ extract_coefficient star_product4.txt 'w_2_1^2'

--- tests/extract_coefficient.cpp
+++ tests/extract_coefficient.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
     if (argc != 3)
     {
-        cout << "Usage: " << argv[0] << " <graph-series-filename> <variable-name>\n";
+        cout << "Usage: " << argv[0] << " <graph-series-filename> <expression>\n";
         return 1;
     }
 
@@ -20,11 +20,11 @@ int main(int argc, char* argv[])
     parser coefficient_reader;
     KontsevichGraphSeries<ex> graph_series = KontsevichGraphSeries<ex>::from_istream(graph_series_file, [&coefficient_reader](std::string s) -> ex { return coefficient_reader(s); });
 
-    ex variable = coefficient_reader(string(argv[2]));
+    ex expression = coefficient_reader(string(argv[2]));
 
     for (size_t n = 0; n <= graph_series.precision(); ++n)
         for (auto& term : graph_series[n])
-            term.first = term.first.coeff(variable);
+            term.first = term.first.coeff(expression);
 
     graph_series.reduce();
 
